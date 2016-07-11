fddd86f97c9ee7d941951599445501e83d031d8a
bmaluenda <bmaluendap@gmail.com>
2016-07-11 10:22:14 -0400

    Added data wrangling for SING substations and their location. Uniformed data formats between systems.

--- python_utility_scripts/db_substations_data_wrangling.py
+++ python_utility_scripts/db_substations_data_wrangling.py
@@ -11,7 +11,8 @@ OCM database. The only inputs needed are the connection parameters.
 """
 from __future__ import print_function
 import pandas as pd
-import sys, os.path, psycopg2, csv
+import sys, os.path, psycopg2
+from csv import writer, reader
 from unidecode import unidecode
 from pyproj import Proj, transform
 
@@ -19,16 +20,22 @@ if sys.version_info > (3, 0):
     import urllib.request as request
 else:
     import urllib as request
+    
+if sys.getdefaultencoding() != 'utf-8':
+    # Character encoding may raise errors if set in ascii or other simple
+    # encodings which do not support spanish characters.
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
 def limpiar(st):
     # Returns a clean string without accents, spaces and commas
     return unidecode(st.replace(' ','_')).lower().replace(
                     ',','_').replace('(','').replace(')','').replace(
-                    "s/e","").strip('_')
+                    "'",'_').replace('s/e','').strip('_')
 
 ###############################
 ############# SIC #############
-"""
+
 # Fetch spreadsheet if it doesn't exist
 if not os.path.isfile('SE_SIC.xls'):
     try:
@@ -37,7 +44,8 @@ if not os.path.isfile('SE_SIC.xls'):
         print ('Successfully downloaded SIC spreadsheet')
     except:
         sys.exit('An error ocurred while fetching the SIC substations spreadsheet')
-        
+
+# Format: northing-easting 
 locations = {
     'lican': ['720623', '5499198', '18'],
     'tap_duqueco': [ '736615.8','5844254.2', '18'],
@@ -113,8 +121,8 @@ tensiones = df_SIC.ix[:,16:23]
 db_voltage=pd.DataFrame(columns=['db_voltage'])
 db_voltage_aux=pd.DataFrame(columns=['db_voltage'])
 prueba=pd.DataFrame(columns=['db_voltage'])
-matriz_aux=pd.DataFrame(columns=['db_name', 'system', 'northing',
-    'easting', 'region', 'cdec_name', 'owner_name','owner_cdec_code', 
+matriz_aux=pd.DataFrame(columns=['db_name', 'system', 'easting',
+    'northing', 'region', 'cdec_name', 'owner_name','owner_cdec_code', 
     'owner_substation_number', 'huso'])
 
 # db_voltage_aux y matriz_aux se utilixan 
@@ -137,10 +145,12 @@ for x in range (1, cdec_name_col.size+1):
     x += 1
 
 #se juntan las matrices modificadas en una matriz
-matriz = pd.concat([db_name_col, system_col, northing_col, easting_col, region_col, cdec_name_col, owner_name_col, owner_cdec_code_col ,owner_substation_number_col, huso_col], axis=1)       
+matriz = pd.concat([db_name_col, system_col, easting_col, northing_col,
+    region_col, cdec_name_col, owner_name_col, owner_cdec_code_col,
+    owner_substation_number_col, huso_col], axis=1)       
 contador_aux=1
 
-matriz_aux.set_value(0, ['db_name', 'system', 'northing', 'easting', 'region', 'cdec_name', 'owner_name','owner_cdec_code', 'owner_substation_number', 'huso'], [0,0,0,0,0,0,0,0,0,0])
+matriz_aux.set_value(0, ['db_name', 'system', 'easting', 'northing', 'region', 'cdec_name', 'owner_name','owner_cdec_code', 'owner_substation_number', 'huso'], [0,0,0,0,0,0,0,0,0,0])
 db_voltage_aux.set_value(0, ['db_voltage'], 0)
 
 #se recorre la matriz, se eliminan las filas sin tensión asignado 
@@ -192,7 +202,7 @@ matriz_aux = matriz_aux[matriz_aux.db_name != 0]
 db_voltage_aux.reindex(index=range(0,len(matriz_aux)))
 
 #Se juntan la matriz y matriz auxiliar para obtner todos los datos de SIC
-matriz = pd.concat([matriz,matriz_aux])
+matriz1 = pd.concat([matriz,matriz_aux])
 matriz['indice']=range(0,len(matriz))
 matriz = matriz.set_index('indice')
 
@@ -200,99 +210,173 @@ with open('substations_sic.csv','w') as outfile:
     matriz.to_csv(outfile, sep=',', header=False, index=False, 
         encoding='utf-8')
 
-"""
+###############################
+############# SING #############
 
-"//////////////////////////// SING //////////////////////////////////"
+# Fetch spreadsheet if it doesn't exist
+if not os.path.isfile('SE_SING.xls'):
+    try:
+        url = 'http://cdec2.cdec-sing.cl/pls/portal/cdec.pck_pag_web_pub.get_file?p_file=SSEE_Coordenadas_UTM30052011.xls'
+        request.urlretrieve(url, 'SE_SING.xls')
+        print ('Successfully downloaded SIC spreadsheet')
+    except:
+        sys.exit('An error ocurred while fetching the SIC substations spreadsheet')
 
-url_sing = 'http://cdec2.cdec-sing.cl/pls/portal/cdec.pck_pag_web_pub.get_file?p_file=SSEE_Coordenadas_UTM30052011.xls'
-urllib.request.urlretrieve(url_sing, "SE_sing.xls")
+def empty_name(cell_value):
+    if cell_value != cell_value:
+        # If its nan, return empty string
+        return ''
+    else:
+        return cell_value
 
-df_SING = pd.read_excel('SE_sing.xls',sheetname=None,skiprows=1)
+# Get dictionary with all sheets as Dataframes
+df_SING = pd.read_excel('SE_SING.xls', sheetname=None, 
+    skiprows=1)
+all_sheets = []
+for key in df_SING:
+    # Drop columns which are not of interest
+    df_SING[key].fillna('', inplace=True)
+    col1 = 'Entrada en Operación'.decode('utf-8')
+    col2 = 'Propietario'
+    if col1 in df_SING[key].columns.values:
+        df_SING[key] = df_SING[key].drop(col1, axis=1)
+    if col2 in df_SING[key].columns.values:
+        df_SING[key] = df_SING[key].drop(col2, axis=1)
+    all_sheets.append(map(list, df_SING[key].values))
+all_stations = []
+# In all_rows, every substation excel row will be stored as a list
+for sheet in all_sheets:
+    for row in sheet:
+        all_stations.append(row)
 
-df_SING_df = pd.DataFrame(columns=['Nombre','Niveles de Tensión (kV)','Entrada en Operación','Coordenada Este', 'Coordenada Norte','Datum','Huso'])
-df_SING_df=df_SING_df.from_dict(df_SING, orient='index', dtype=None)
+# Coordinate projection engines
+projection_UTM18S = Proj('+init=EPSG:32718')
+projection_UTM19S = Proj('+init=EPSG:32719')
+projection_PSAD56 = Proj('+init=EPSG:24879')
 
+# CSV columns
+name = 0
+voltage = 1
+easting = 2
+northing = 3
+datum = 4
+zone = 5
+original_name = 6
 
+empty = 0
+for index, station in enumerate(all_stations):
+    # Tensions are not inputted in two cases: when the company is listing
+    # all coordinates in the station's terrain or when all the voltage levels
+    # are specified in the first row of the station. In both cases, rows w/o
+    # voltage values should be removed.
 
-# db_name_sing_col = pd.DataFrame(columns=['db_name'])
-# system_sing_col = pd.DataFrame(columns=['system'])
+    # Some substations correspond to the same location with different
+    # voltage levels, so names must be filled.
+    if not station[name]:
+        # Fetch the previous name if an empty value is found
+        station[name] = all_stations[index-1][name]
+        station[easting] = all_stations[index-1][easting]
+        station[northing] = all_stations[index-1][northing]
+        station[datum] = all_stations[index-1][datum]
+        station[zone] = all_stations[index-1][zone]
+    # Put the original name at the end of the list and save a clean copy
+    station.append(station[name])
+    station[name] = limpiar(station[name])
 
-# cdec_name_sing_col = df_SING.ix[:,0:1]
-# cdec_name_sing_col.columns = ['cdec_name']
+    
+    # In these previous cases, voltages are defined as a string in the 
+    # first row of the station. Parse these voltages and distribute them:
+    if isinstance(station[voltage], basestring):
+        voltages = station[voltage].split('/')
+        voltages = [v.strip() for v in voltages]
+        for i, v in enumerate(voltages):
+            all_stations[index+i][voltage] = v
+    
+    # Some substations have coordinates for all structures in a station
+    # Keep only 1 pair of coords per voltage
+    if not station[voltage]:
+        empty+=1
+    
+    # If the station has no defined Datum, then it is wgs84, zone 19.
+    # (Determined by manual inspection)
+    if not station[datum] or station[datum] == 'K':
+        station[datum] = 'WGS84'
+    if not station[zone]:
+        station[zone] = 19
+    # Also, transform floats to strings
+    if not isinstance(station[datum], basestring):
+        station[datum] = 'WGS84'
+    # Some coordinates are mixed up (northing should always be greater
+    # than easting for zone 19 and the SING)
+    if station[easting] > station[northing]:
+        station[easting], station[northing] = station[northing], station[easting]
+        
+    # Only one substation doesn't have coordinates:
+    if station[name] == 'muelle':
+        station[easting] = '368548'
+        station[northing] = '7486686'
+        station[datum] = 'WGS84'
+        station[zone] = 19
+    
+    # Transform PSAD56 coordinates into WGS84    
+    if '84' not in station[datum]:
+        station[easting], station[northing] = (str(coord) 
+            for coord in transform(
+                projection_PSAD56, projection_UTM18S, 
+                float(station[easting]), float(station[northing])))
+        station[zone] = '18'
+# Loop through the list removing repeated stations
+while empty > 0:
+    for st in all_stations:
+        if not st[voltage]:
+            all_stations.remove(st)
+            empty-=1
+            break
 
-# northing_sing_col = df_SING.ix[:,5:6]
-# northing_sing_col.columns = ['northing']
-
-# easting_sing_col = df_SING.ix[:,4:5]
-# easting_sing_col.columns = ['easting']
-
-# region_sing_col=pd.DataFrame(columns=['region'])
-
-# owner_name_sing_col = df_SING.ix[:,1:2]
-# owner_name_sing_col.columns = ['owner_name']
-
-# owner_cdec_code_sing_col =pd.DataFrame(columns=['owner_cdec_code'])
-
-
-# owner_substation_number_sing_col= pd.DataFrame(columns=['owner_substation_number'])
-
-# huso_sing_col = df_SING.ix[:,7:8]
-# huso_sing_col.columns = ['huso']
-# huso_sing_col = huso_sing_col.astype(int)
-
-# for x in range (1, cdec_name_sing_col_col.size+1):
-#   stringA=cdec_name_sing_col['cdec_name'][x-1:x].to_string(index=False)
-#   stringB=stringA.replace(" ","_").replace("(","").replace(")","").replace("á","a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u").lower();
-#   db_name_sing_col.set_value(x-1, ['db_name'], stringB)
-
-#   system_sing_col.set_value(x-1, ['system'], 'sing')
-#   region_sing_col.set_value(x-1, ['region'], 'NA')
-#   owner_cdec_code_sing_col.set_value(x-1, ['owner_cdec_code'], 'NA')
-#   owner_substation_number_sing_col.set_value(x-1, ['owner_substation_number'], 'NA')
-
-#   stringC=northing_sing_col['northing'][x-1:x].to_string(index=False).replace(",",".")
-#   northing_sing_col.set_value(x-1, ['northing'],stringC)
-
-#   stringD=easting_sing_col['easting'][x-1:x].to_string(index=False).replace(",",".")
-#   easting_sing_col.set_value(x-1, ['easting'],stringD)
-
-#   x += 1
-
-
-# matriz_sing = pd.concat([db_name_sing_col, system_sing_col, northing_sing_col, easting_sing_col, region_sing_col, cdec_name_sing_col, owner_name_sing_col, owner_cdec_code_sing_col ,owner_substation_number_sing_col, huso_sing_col], axis=1)      
-
-#print(matriz_sing)
+with open('substations_sing.csv', 'w') as out:
+    csv_writer = writer(out, delimiter = ',')
+    for station in all_stations:
+        csv_writer.writerow([station[name], station[voltage], 'sing',
+            station[easting], station[northing], '', station[original_name],
+            '', '', '', station[zone]])
 
 ##############################
 ####### UPLOAD TO DB #########
-"""
+
 # Both csv's must be read into a big list of lists
 subs_for_db = []
 with open('substations_sic.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
+    read = reader(f)
+    for row in read:
+        subs_for_db.append(row)
+with open('substations_sing.csv', 'r') as f:
+    read = reader(f)
+    for row in read:
         subs_for_db.append(row)
 
 # Proyection engines to transform UTM coordinates into a single zone: 18S.
 # Substations which are missing location are given manually inputted values.
-projection_UTM18S = Proj('+init=EPSG:32718')
-projection_UTM19S = Proj('+init=EPSG:32719')
 for station in subs_for_db:
     if station[-1] == '0':
-        # Northing
-        station[3] == locations[station[0]][1]
         # Easting
-        station[4] == locations[station[0]][0]
+        station[3] = locations[station[0]][1]
+        # Northing
+        station[4] = locations[station[0]][0]
         # Zone
-        station[-1] == locations[station[0]][2]
+        station[-1] = locations[station[0]][2]
     # Project all coordinates to UTM zone 18S.
     if station[-1] == '19':
-        station[4], station[3] = (str(coord) for coord in transform(
+        station[3], station[4] = (str(coord) for coord in transform(
             projection_UTM19S, projection_UTM18S, 
-            station[4], station[3]))
+            station[3], station[4]))
         station[-1] = '18'
-
-#se conecta con el servidor
+with open('testing.csv', 'w') as out:
+    csv_writer = writer(out, delimiter = ',')
+    for station in subs_for_db:
+        csv_writer.writerow(station)
+"""
+##############
+# DB Conection
 try:
     # Remember to enter and/or modify connection parameters accordingly to your
     # setup
